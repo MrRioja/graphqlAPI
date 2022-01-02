2d24e969e4d589387cfe22164a61079f0035619f
Luiz Rioja <lulyrioja@gmail.com>
2022-01-01 21:05:46 -0300

    :alien: Refactoring server creation

--- src/startServer.js
+++ src/startServer.js
@@ -1,17 +1,51 @@
-import { ApolloServer, PubSub } from "apollo-server";
+import express from "express";
 import mongoose from "mongoose";
+import { createServer } from "http";
+import { execute, subscribe } from "graphql";
+import { ApolloServer } from "apollo-server-express";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { SubscriptionServer } from "subscriptions-transport-ws";
 
-function startServer({ typeDefs, resolvers }) {
+async function startServer({ typeDefs, resolvers }) {
+  const PORT = 4000;
+  const app = express();
+  const httpServer = createServer(app);
+
   mongoose.connect("mongodb://localhost:27017/graphql", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
 
-  const pubsub = new PubSub();
-  const server = new ApolloServer({ typeDefs, resolvers, context: { pubsub } });
-  server
-    .listen()
-    .then(({ url }) => console.log(`🔥 Server is running on ${url}`));
+  const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers,
+  });
+
+  const subscriptionServer = SubscriptionServer.create(
+    { schema, execute, subscribe },
+    { server: httpServer, path: "/graphql" }
+  );
+
+  const server = new ApolloServer({
+    schema,
+    plugins: [
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              subscriptionServer.close();
+            },
+          };
+        },
+      },
+    ],
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+
+  httpServer.listen(PORT, () =>
+    console.log(`🔥 Server is now running on http://localhost:${PORT}/graphql`)
+  );
 }
 
 export default startServer;
